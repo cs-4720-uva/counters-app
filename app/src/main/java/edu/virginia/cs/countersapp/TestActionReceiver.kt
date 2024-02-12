38507c85f072dcff711c6d9dd474733a67a98cb5
WillMcBurney <pmcburn1@gmail.com>
2024-02-12 11:51:16 -0500

    Added Notification support

--- app/src/main/java/edu/virginia/cs/countersapp/TestActionReceiver.kt
+++ app/src/main/java/edu/virginia/cs/countersapp/TestActionReceiver.kt
@@ -1,15 +1,31 @@
 package edu.virginia.cs.countersapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service.START_STICKY
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 
 class TestActionReceiver : BroadcastReceiver() {
+    var notificationStarted = false
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        if (intent.action == "TEST_ACTION") {
+        if (intent.action == "TEST_ACTION" && !notificationStarted) {
+            val intent = Intent(context, MyNotificationService::class.java)
+            context.startForegroundService(intent)
             println("Received Test Action")
+            notificationStarted = true
+        } else if (intent.action == "TEST_ACTION" && notificationStarted) {
+            val intent = Intent(context, MyNotificationService::class.java)
+            context.stopService(intent)
+            notificationStarted = false
         }
     }
 }
